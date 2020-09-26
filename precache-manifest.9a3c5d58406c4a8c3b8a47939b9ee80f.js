bb6b725afed8171472035d0ed73f514c50cee0a1
kabdelka <ksabdelka@gmail.com>
2020-09-26 09:29:08 +0100

    Updates

---
+++ precache-manifest.9a3c5d58406c4a8c3b8a47939b9ee80f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952876be9cdacce750a023709a4c243d",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "8924f94c2b823f5e25fc",
+    "url": "/weatherapp/static/css/main.026fb161.chunk.css"
+  },
+  {
+    "revision": "35b55f6cca168766438a",
+    "url": "/weatherapp/static/js/2.8a9fcb99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.8a9fcb99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8924f94c2b823f5e25fc",
+    "url": "/weatherapp/static/js/main.f7db9c87.chunk.js"
+  },
+  {
+    "revision": "8d52fea309e5ea175bc9",
+    "url": "/weatherapp/static/js/runtime-main.8306bc01.js"
+  },
+  {
+    "revision": "0f830d539facd08dcc2cb7870abdd870",
+    "url": "/weatherapp/static/media/salat.0f830d53.svg"
+  }
+]);
